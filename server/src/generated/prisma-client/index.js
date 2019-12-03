3fecb5f8ee2c362f45a526045777b1487fa12816
AssisrMatheus <assisr.matheus@gmail.com>
2019-12-03 18:42:57 -0300

    Finished server setup

--- server/src/generated/prisma-client/index.js
+++ server/src/generated/prisma-client/index.js
@@ -1,40 +1,25 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var prisma_lib_1 = require("prisma-client-lib");
-var typeDefs = require("./prisma-schema").typeDefs;
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var prisma_lib_1 = require('prisma-client-lib');
+var typeDefs = require('./prisma-schema').typeDefs;
 
 var models = [
   {
-    name: "Link",
+    name: 'Link',
     embedded: false
   },
   {
-    name: "User",
+    name: 'User',
     embedded: false
   },
   {
-    name: "Vote",
+    name: 'Vote',
     embedded: false
   }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: ``,
-  secret: `mysecret123`
+  endpoint: process.env.PRISMA_HOST ? process.env.PRISMA_HOST : `http://localhost:4466`
 });
 exports.prisma = new exports.Prisma();
-var models = [
-  {
-    name: "Link",
-    embedded: false
-  },
-  {
-    name: "User",
-    embedded: false
-  },
-  {
-    name: "Vote",
-    embedded: false
-  }
-];
